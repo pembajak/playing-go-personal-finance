fb609a1ffe3b9cc23808302d75c306eee2be4fe6
yosef <yosef.sugiarto@gmail.com>
2022-09-29 19:42:50 +0700

    chore: update user validation

--- internal/app/models/User.go
+++ internal/app/models/User.go
@@ -8,8 +8,8 @@ import (
 
 type User struct {
 	ID        int64          `json:"id" deepcopier:"ID"`
-	Email     string         `json:"email" deepcopier:"email" validate:"required,email,min=10,max=100"`
-	FullName  string         `json:"full_name" deepcopier:"FullName" validate:"required,min=10,max=100"`
+	Email     string         `json:"email" deepcopier:"email" validate:"required,email,min=1,max=100"`
+	FullName  string         `json:"full_name" deepcopier:"FullName" validate:"required,min=1,max=100"`
 	Phone     string         `json:"phone" deepcopier:"Phone" validate:"required,min=8,max=16"`
 	Password  string         `json:"password,omitempty" deepcopier:"Password" validate:"required,min=5,max=20"`
 	CreatedAt time.Time      `json:"created_at"`
